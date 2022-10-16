c5621a8cb8b712c40d09737eb4dfe52154483086
Alexis <alexislegeay@hotmail.fr>
2022-10-16 21:33:50 +0200

    update bot

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const { App } = require("@slack/bolt");
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 require("dotenv").config();
+const ZONE = require('./geopoints.json')
 // Initializes your app with your bot token and signing secret
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -11,6 +21,18 @@ const app = new App({
 app.command("/miam", async ({ command, ack, say }) => {
     try {
       await ack();
+      console.log(ZONE)
+      let txt = command.text // The inputted parameters
+      let resto = await authorize().then(listRestaurant).catch(console.error);
+      if(ZONE.zoneNantes.find(element => element.zoneName == txt)) {
+          say("Bip... Boop ! Recherche d'un coin pour manger en cours vers " + txt)
+          resto.forEach((row) => {
+            console.log(`${row[1]} ${row[2]}`);
+          });
+      } else {
+          say("Zone non reconnu :'( ")
+      }
+
       say("Miam le bot fonctionne !");
     } catch (error) {
         console.log("err")
@@ -25,3 +47,57 @@ app.command("/miam", async ({ command, ack, say }) => {
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
+
+
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+async function listRestaurant(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: 'A2:C',
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Major:');
+  return rows
+}
